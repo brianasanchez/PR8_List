08215e0ef1ab2b5c2bea8c539f9c2457c9c10b42
brianasanchez <a.23308051281213@cbtis128.edu.mx>
2024-09-25 20:39:03 -0600

    Add files via upload

---
+++ PR8_Listas/PR8_Listas.py
@@ -0,0 +1,60 @@
+print(" ") #Print imprime un espacio
+
+print("Briana Sarahi Sanchez Perez 3-W, PR8_Listas")
+print(" ") #Print imprime un espacio
+
+#Practica 8  donde usamos listas
+thislist = ["100", "200", "300"] #Da valores a la lista
+print(thislist) # Muestra el miembro de la lista
+
+#cuando usamos (len( )) te despliega
+#el numero de elementos de la lista
+thislist2 = ["sandia", "pera", "uva"]
+print(len(thislist))
+
+#Separa con un espacio
+print(" ")
+#Da valores a varias listas
+list1 = ["libro", "cuaderno", "lapiz"]
+list2 = [7, 14, 21, 28, 35]
+list3 = ["abcdef", 70, True, 120, "holuuu"]
+
+#Muestra los miembros de cada lista
+print(list1)
+print(list2)
+print(list3)
+
+print(" ") # Printi imprime un espacio
+
+thislist = ["computadora", "mouse", "teclado"] # Da valores a la lista
+
+#Muestra el miembro 1 y comienza en Cero 0
+print(thislist[1])
+
+#Muestra el ultimo miembro de la lisa
+print(thislist[-1])
+
+print(" ") # Print imprime un espacio
+
+thislist = ["manzana", "banana", "pepino"]
+
+#Muestra el miembro 1 y comienza en Cero 0
+print(thislist[1])
+
+#Muestra el ultimo miembro de la lisa
+print(thislist[-1])
+
+thislist = ["guayaba", "melon", "fresa", "mandarina", "kiwi", "mango", "naranja"] 
+# Da valores a la lista
+print(thislist[1:6]) 
+# el 1:6 muestra a patir de la posicion 2 a la 5 de los miembros de la lista
+print(thislist[-1-4])
+#Muestra el miembro de atras para adelante de la lisa
+
+print(" ") # print imprime un espacio
+
+thislist = ["1", "2", "3", "4", "5", "6", "7", "8"] # Da valores a la lista
+print(thislist[5:8]) 
+# el 5:8 muestra a patir de la posicion 2 a la 5 de los miembros de la lista
+print(thislist[-3*2]) 
+#Muestra el miembro de atras para adelante de la lisa
